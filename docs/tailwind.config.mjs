211fefd49879e1e85d8ec87d0cc0a7070772e34a
Bean.Wei <beanwei@126.com>
2024-11-22 22:53:56 +0800

    feat: accordion component, refactor tailwind config

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,20 +1,15 @@
 import starlight from '@astrojs/starlight-tailwind'
 import astroui from '@beanwei/astro-ui/tailwind'
-import animate from 'tailwindcss-animate'
 import { isolateInsideOfContainer, scopedPreflightStyles } from 'tailwindcss-scoped-preflight'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+	presets: [astroui()],
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/@beanwei/astro-ui/src/**/*.{astro,ts}',
 	],
-	theme: {
-		extend: {},
-	},
 	plugins: [
-		animate,
-		astroui,
 		starlight(),
 		scopedPreflightStyles({
 			isolationStrategy: isolateInsideOfContainer('[data-part]'),
---
+++ src/utils/uniqId.ts
@@ -0,0 +1,6 @@
+let i = 0
+
+export default (prefix: string) => {
+	const id = `${++i}`
+	return prefix ? prefix + id : id
+}
