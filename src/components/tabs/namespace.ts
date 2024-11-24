f5e0e223ebdb3916fbaa48c3040d761e0e57388d
Bean.Wei <beanwei@126.com>
2024-11-24 09:30:21 +0800

    feat: tabs

---
+++ src/components/tabs/namespace.ts
@@ -0,0 +1,4 @@
+export { default as Root } from './Tabs.astro'
+export { default as List } from './TabsList.astro'
+export { default as Trigger } from './TabsTrigger.astro'
+export { default as Content } from './TabsContent.astro'
